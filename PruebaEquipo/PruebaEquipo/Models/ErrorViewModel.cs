8e8f7a2587ae0b019723fd31688377d170185032
Sidney Rojas <sidney.rojas@outlook.com>
2019-03-15 09:44:19 -0600

    Agregar Proyecto

---
+++ PruebaEquipo/PruebaEquipo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaEquipo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
